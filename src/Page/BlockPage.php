02206846d2eab603bc1c128ec3ada9e6932d918f
Stephanus Yanaputra <stephanus.yanaputra@gmail.com>
2020-03-22 19:13:52 +1100

    Add new Blockpage, Introduce Elemental, Housekeeping

---
+++ src/Page/BlockPage.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Syanaputra\Clio\Page;
+
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\DB;
+use SilverStripe\Security\Permission;
+use SilverStripe\Security\PermissionProvider;
+use SilverStripe\Security\Security;
+use SilverStripe\View\TemplateGlobalProvider;
+use Syanaputra\Clio\Admin\CMSFields\HeaderCMSFieldsModifier;
+
+/**
+ * Class BlockPage
+ *
+ * @package Syanaputra\Clio\Page
+ */
+class BlockPage extends SiteTree
+{
+    /**
+     * @var null
+     */
+    private static $_cached_settings = null;
+
+    /**
+     * @var array
+     */
+    private static $db = [];
+
+    /**
+     * @var array
+     */
+    private static $has_one = [];
+
+    /**
+     * @var array
+     */
+    private static $many_many = [];
+
+    /**
+     * @var array
+     */
+    private static $many_many_extraFields = [];
+
+    /**
+     * @var string
+     */
+    private static $singular_name = 'Block Page';
+
+    /**
+     * @var string
+     */
+    private static $plural_name = 'Block Pages';
+
+    /**
+     * @var string
+     */
+    private static $table_name = 'BlockPages';
+
+    /**
+     * @var string
+     */
+    private static $description = 'Clio Default Page with lots of customised blocks';
+
+    /**
+     * @var array
+     */
+    private static $indexes = [];
+
+    /**
+     * @var array
+     */
+    private static $searchable_fields = [];
+
+    /**
+     * @var array
+     */
+    private static $extensions = [];
+
+    /**
+     * @var array
+     */
+    private static $defaults = [];
+
+    /**
+     * Update the CMS Fields
+     *
+     * @return \SilverStripe\Forms\FieldList
+     */
+    public function getCMSFields()
+    {
+        $this->beforeUpdateCMSFields(function (FieldList $fields) {
+            $fields->removeByName(['Content']);
+        });
+
+        return parent::getCMSFields();
+    }
+
+}
